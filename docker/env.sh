088b5e99b04798d25e15b7e257f7837792f8a803
Kevin Leyow <kleyow@gmail.com>
2025-04-22 10:25:45 -0500

    chore: ci

--- docker/env.sh
+++ docker/env.sh
@@ -13,3 +13,10 @@ get_external_ip() {
 
 # set/override dynamic variables
 export REDIS_CLUSTER_ANNOUNCE_IP=$(get_external_ip)
+export TEST_TTK_SVC_VERSION=v18.7.4
+export TEST_TTK_UI_VERSION=v16.0.4
+export TEST_TTK_CLI_VERSION=v1.5.0
+
+export DEP_KAFKA_VERSION=3.5.0
+export DEP_MYSQL_VERSION=8.0
+export DEP_MONGO_VERSION=6.0.5
--- test/scripts/test-integration.sh
+++ test/scripts/test-integration.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-export TEST_TTK_SVC_VERSION=v18.7.4
-export TEST_TTK_UI_VERSION=v16.0.4
-export TEST_TTK_CLI_VERSION=v1.5.0
-
-export DEP_KAFKA_VERSION=3.5.0
-export DEP_MYSQL_VERSION=8.0
-export DEP_MONGO_VERSION=6.0.5
-
 source ./docker/env.sh
 
 #docker load -i /tmp/docker-image.tar
